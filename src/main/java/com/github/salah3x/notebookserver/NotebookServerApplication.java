69efab608be7b99cf24d751c91496c3b69e49b7e
salah3x <salah.loukili@gmail.com>
2019-10-07 21:28:22 +0100

    Initial commit

---
+++ src/main/java/com/github/salah3x/notebookserver/NotebookServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.salah3x.notebookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookServerApplication.class, args);
+    }
+
+}
